afe22c634ebfe16d2a43b214ae9e5106cc6e3e24
Gerardo <74930791+ChopperNakama@users.noreply.github.com>
2024-11-02 17:13:00 -0500

    Add files via upload
    Codigo que me permite de una matriz 1D escojer 2 numeros que sumados me dan un target, muy interesante en c#

--- Program.cs
+++ Program.cs
@@ -1,53 +1,44 @@
-﻿using Sobrecarga_caso_estudio_02;
-using System;
-namespace Sobrecarga_caso_estudio_02
+﻿int target = 60;
+int[] coins = new int[] {5, 5, 50, 25, 25, 10, 5};
+int[,] result = TwoCoins(coins, target);
+
+if (result.Length == 0) 
 {
-    public class Complejo
+    Console.WriteLine("No two coins make change");
+} 
+else 
+{
+    Console.WriteLine("Change found at positions:");
+    for (int i = 0; i < result.GetLength(0); i++) 
     {
-        private double real;
-        private double imaginario;
-        public Complejo(double re, double im)
+        if (result[i,0] == -1) 
         {
-            real = re; imaginario = im;
+            break;
         }
-        public void AsignarComplejo(double re, double im)
-        {
-            real = re; imaginario = im;
-        }
-        public static Complejo operator +(Complejo a, Complejo b)
-        {
-            return new Complejo(a.real + b.real, a.imaginario + b.imaginario);
-        }
-        public static Complejo operator -(Complejo a, Complejo b)
-        {
-            return new Complejo(a.real - b.real, a.imaginario - b.imaginario);
-        }
-        public override string ToString()
-        {
-            return (String.Format("{0}" + (imaginario >= 0 ? "+" : "") + "{1}i", real, imaginario));
+        Console.WriteLine($"{result[i,0]},{result[i,1]}");
+    }
+}
 
+int[,] TwoCoins(int[] coins, int target) 
+{
+    int[,] result = {{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1}};
+    int count = 0;
+
+    for (int curr = 0; curr < coins.Length; curr++) 
+    {
+        for (int next = curr + 1; next < coins.Length; next++) 
+        {    
+            if (coins[curr] + coins[next] == target) 
+            {
+                result[count, 0] = curr;
+                result[count, 1] = next;
+                count++;
+            }
+            if (count == result.GetLength(0)) 
+            {
+                return result;
+            }
         }
     }
-    class Program
-    {
-        public static void Main(string[] args)
-        {
-            Complejo a = new Complejo(1.2, 2);
-            Complejo b;
-            Complejo c = new Complejo(1.5, 2);
-            Complejo d;
-            double real, imaginario;
-            Console.WriteLine("Numero complejo: ");
-            Console.WriteLine("re: ");
-            real = double.Parse(Console.ReadLine());
-            Console.WriteLine("im: ");
-            imaginario = double.Parse(Console.ReadLine());
-            a.AsignarComplejo(real, imaginario);
-            b = a;
-            d = a + b - c;
-            d = d + new Complejo(5, 2);
-            Console.WriteLine(d.ToString());
-        }
-    }
-
+    return (count == 0) ? new int[0,0] : result;
 }
